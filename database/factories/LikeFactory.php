bdf7ae52f7a913e95013f8249d81add225f8c941
bayutirto <bang.bayutirto@gmail.com>
2020-03-16 00:10:46 +0700

    Factory dan Seed

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -2,11 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\User;
 use App\Models\Like;
 use Faker\Generator as Faker;
 
 $factory->define(Like::class, function (Faker $faker) {
     return [
-        //
+        'user_id'=> function(){
+            return User::all()->random();
+        }
     ];
 });
