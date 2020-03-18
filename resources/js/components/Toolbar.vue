01c93029333ca47a638740cc2caa88b17c2431a4
bayutirto <bang.bayutirto@gmail.com>
2020-03-18 15:33:31 +0700

    Initial Frontend Components

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,28 @@
+<template>
+     <v-toolbar class="blue darken-2 white--text">
+
+        <v-toolbar-title class="mx-4">Forum App</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+          <v-btn tile text class="ma-2 white--text"><span>Forum</span></v-btn>
+
+          <v-btn tile text class="ma-2 white--text"><span>Ask Question</span></v-btn>
+
+          <v-btn tile text class="ma-2 white--text"><span>Category</span></v-btn>
+
+          <v-btn tile class="ma-2 white--text" outlined color="white">Login</v-btn>
+
+      </v-toolbar>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
